48601ea09c0ae5a5b8b0685169e23ada75d48474
I2rys <i2rys@protonmail.com>
2022-07-25 15:44:40 +0800

    init

---
+++ README.md
@@ -0,0 +1,32 @@
+# MassGitLeaker
+Fast & Lightweight Github user repositories/repos credential leaker with [trufflehog](https://github.com/trufflesecurity/trufflehog).
+
+## Installation
+Github:
+```
+git clone https://github.com/OTAKKATO/massgitleaker
+```
+
+NpmJS:
+```
+npm i argparse parallel-park request-async shelljs path fs
+```
+
+## Usage
+```
+usage: index.js [-h] [-u USER] [-r REPOS] -o OUTPUTDIR
+
+You must choose at least one argument between --user & --repos
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -u USER, --user USER  Scan all of the repositories of the specified
+                        user.
+  -r REPOS, --repos REPOS
+                        Scan the listed repositories.
+  -o OUTPUTDIR, --outputDir OUTPUTDIR
+                        Results directory output.
+```
+
+## License
+MIT © I2rys
---
+++ index.js
@@ -0,0 +1,92 @@
+(async()=>{
+    "use strict";
+
+    // Dependencies
+    const { ArgumentParser } = require("argparse")
+    const { runJobs } = require("parallel-park")
+    const request = require("request-async")
+    const shellJS = require("shelljs")
+    const path = require("path")
+    const fs = require("fs")
+    
+    // Variables
+    const parser = new ArgumentParser({
+        description: "You must choose at least one argument between --user & --repos"
+    })
+
+    var MassGitLeak = {
+        userRepos: [],
+        page: 1,
+        found: 0
+    }
+    
+    var args;
+
+    // Functions
+    async function getUserRepositories(){
+        var response = await request(`https://api.github.com/users/${args.user}/repos?per_page=50&page=${MassGitLeak.page}`, {
+            headers: {
+                "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.134 Safari/537.36"
+            }
+        })
+
+        response = JSON.parse(response.body)
+
+        if(!response.length){
+            if(!MassGitLeak.userRepos.length) return console.log("No repositories found in the user.")
+
+            console.log(`${MassGitLeak.userRepos.length} repositories found in the user.`)
+            console.log("Scanning the user repositories, please wait.")
+
+            return scanRepositories(MassGitLeak.userRepos)
+        }
+
+        for( const repo of response ) MassGitLeak.userRepos.push(repo.html_url)
+
+        MassGitLeak.page++
+        getUserRepositories()
+    }
+
+    async function scanRepositories(repos){
+        await runJobs(
+            repos,
+            async(repo)=>{
+                var results = shellJS.exec(`tools\\trufflehog git ${repo} --no-update`, { silent: true }).stdout
+    
+                if(results.match("Detector Type:")){
+                    MassGitLeak.found++
+                    console.log(`Found credentials in ${repo}`)
+                    fs.writeFileSync(`${path.join(args.outputDir, repo.slice(19, repo.length).replace(".git", "").replace(/\//g, "-"))}.txt`, results, "utf8")
+                }
+            }
+        )
+    
+        MassGitLeak.found ? console.log(`Found credentials in ${MassGitLeak.found} repositories.`) : console.log("No credentials found in repositories.")
+    }
+    
+    // Main
+    parser.add_argument("-u", "--user", { help: "Scan all of the repositories of the specified user." })
+    parser.add_argument("-r", "--repos", { help: "Scan the listed repositories." })
+    parser.add_argument("-o", "--outputDir", { help: "Results directory output.", required: true })
+
+    args = parser.parse_args()
+
+    if(!fs.existsSync(args.outputDir)) return console.log("Invalid outputDir.")
+    
+    if(args.user){
+        console.log("Scraping the user repositories, please wait.")
+
+        getUserRepositories()
+    }else if(args.repos){
+        if(!fs.existsSync(args.repos)) return console.log("Invalid repos.")
+    
+        const repos = fs.readFileSync(args.repos, "utf8").replace(/\r/g, "").split("\n")
+        
+        if(!repos.length) return console.log("repositories data is empty.")
+        
+        console.log("Scanning the repositories, please wait.")
+        scanRepositories(repos)
+    }else{
+        console.log("Please choose at least 1 argument between --user & --repos")
+    }
+})()
